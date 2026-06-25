35370de5efdcce280292dcc20b6404a8ab85567a
b7gkpsd5jt-tech <b7gkpsd5jt@privaterelay.appleid.com>
2026-06-25 13:14:57 +0200

    Create next.config.ts

---
+++ client/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [{ protocol: 'http', hostname: 'localhost', port: '4000', pathname: '/uploads/**' }],
+  },
+};
+
+export default nextConfig;
